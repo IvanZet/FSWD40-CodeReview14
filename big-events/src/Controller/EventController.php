200fc7595c93729b04e9bf8eca31842847a13d13
Ivan Zykov <ivan.v.zykov@gmail.com>
2018-07-21 13:25:09 +0200

    Adds functionality to add new event - Updates links to pages using Twig's path() tag - Installs form package - Adds new action addEvent with form builder and saving to DB - Adds new page add_event.html.twig with form

--- big-events/src/Controller/EventController.php
+++ big-events/src/Controller/EventController.php
@@ -1,6 +1,11 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Event;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,7 +29,7 @@ class EventController extends Controller {
 	}
 
 	/**
-	 * @Route("details/{id}", name="details")
+	 * @Route("/details/{id}", name="details")
 	 */
 	public function showDetails ($id) {
 		$details = $this->getDoctrine()->getRepository(Event::class)->find($id);
@@ -34,5 +39,56 @@ class EventController extends Controller {
 		}
 
 		return $this->render('events/show_details.html.twig', ['details' => $details]);
+	}
+
+	/**
+	 * @Route ("/add", name="add")
+	 */
+	public function addEvent (Request $request) {
+
+		$event = new Event();
+
+		//Writing input fields here manually
+		/*$event->setName('Lorem ipsum');
+		$event->setImage('http://via.placeholder.com/350x150');
+		$event->setDescription('Curabitur in blandit ante. Aliquam eu urna eleifend nunc rhoncus dapibus ac at erat. Pellentesque sed ligula ligula. Praesent velit enim, efficitur non mi non, accumsan pellentesque metus. Sed maximus tempor porta. Mauris accumsan massa nec aliquam lacinia. Aliquam ac augue sit amet elit bibendum varius a sed ex. Suspendisse et facilisis urna. Praesent finibus nunc viverra tellus porttitor ultricies in at mi. Duis accumsan volutpat ipsum, ultrices laoreet risus.');
+		$event->setDateTime(new \DateTime('tomorrow'));
+		$event->setCapacity(100);
+		$event->setAddress('12 Meiereistraße, 1020 Wien');
+		$event->setUrl('https://www.lipsum.com');
+		$event->setPhoneNumber('+43 600 5554444');
+		$event->setEmail('event@mail.com');
+		$event->setType('theatre');*/
+
+		$form = $this->createFormBuilder($event)
+			->add('name', TextType::class)
+			->add('image', TextType::class)
+			->add('description', TextType::class)
+			->add('datetime', DateType::class)
+			->add('capacity', IntegerType::class)
+			->add('address', TextType::class)
+			->add('url', TextType::class)
+			->add('phoneNumber', TextType::class)
+			->add('email', TextType::class)
+			->add('type', TextType::class)
+			->add('save', SubmitType::class, array('label' => 'Create event'))
+			->getForm();
+		
+			$form->handleRequest($request);
+
+			if ($form->isSubmitted()) { //!!! && $form->isValid()
+				// $form->getData() holds the submitted values
+        // but, the original `$event` variable has also been updated
+        $event = $form->getData();
+
+      // Save the event to the database
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($event);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('events');
+			}
+
+		return $this->render('events/add_event.html.twig', array('form' => $form->createView()));
 	}
 }
