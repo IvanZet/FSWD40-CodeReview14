4724ad11024f6be62ef7137119c26930fbdd8742
Ivan Zykov <ivan.v.zykov@gmail.com>
2018-07-21 16:59:29 +0200

    Adds functionality to edit event

--- big-events/src/Controller/EventController.php
+++ big-events/src/Controller/EventController.php
@@ -17,7 +17,7 @@ class EventController extends Controller {
 	/**
 	 * @Route("/events", name="events")
 	 */
-	public function showAll () {
+	public function showAllAction() {
 		$repository = $this->getDoctrine()->getRepository(Event::class);
 		$events = $repository->findAll();
 
@@ -28,11 +28,16 @@ class EventController extends Controller {
 		return $this->render('events/show_events.html.twig', ['events' => $events]);
 	}
 
+	//Returns details of an event user in actions showDetails and edit
+	public function getDetails($id) {
+		return $this->getDoctrine()->getRepository(Event::class)->find($id);
+	}
+
 	/**
 	 * @Route("/details/{id}", name="details")
 	 */
-	public function showDetails ($id) {
-		$details = $this->getDoctrine()->getRepository(Event::class)->find($id);
+	public function showDetailsAction($id) {
+		$details = $this->getDetails($id);
 		
 		if (!$details) {
 			throw $this->createNotFoundException('Event not found for ID: ' . $id);
@@ -41,10 +46,32 @@ class EventController extends Controller {
 		return $this->render('events/show_details.html.twig', ['details' => $details]);
 	}
 
+	//Creates form for event
+	public function createEventForm(&$event, $label) {
+		return $this->createFormBuilder($event)
+			->add('name', TextType::class)
+			->add('image', TextType::class)
+			->add('description', TextType::class)
+			->add('datetime', DateType::class)
+			->add('capacity', IntegerType::class)
+			->add('address', TextType::class)
+			->add('url', TextType::class)
+			->add('phoneNumber', TextType::class)
+			->add('email', TextType::class)
+			->add('type', TextType::class)
+			->add('save', SubmitType::class, array('label' => "$label"))
+			->getForm();
+	}
+
+	//Handles request
+	public function doRequest(&$form, &$request) {
+		return $form->handleRequest($request);
+	}
+
 	/**
 	 * @Route ("/add", name="add")
 	 */
-	public function addEvent (Request $request) {
+	public function addAction(Request $request) {
 
 		$event = new Event();
 
@@ -60,7 +87,7 @@ class EventController extends Controller {
 		$event->setEmail('event@mail.com');
 		$event->setType('theatre');*/
 
-		$form = $this->createFormBuilder($event)
+		/*$form = $this->createFormBuilder($event)
 			->add('name', TextType::class)
 			->add('image', TextType::class)
 			->add('description', TextType::class)
@@ -72,23 +99,101 @@ class EventController extends Controller {
 			->add('email', TextType::class)
 			->add('type', TextType::class)
 			->add('save', SubmitType::class, array('label' => 'Create event'))
-			->getForm();
-		
-			$form->handleRequest($request);
+			->getForm();*/
 
-			if ($form->isSubmitted()) { //!!! && $form->isValid()
+			/*$form->handleRequest($request);*/
+
+			$form = $this->createEventForm($event, 'Create event');
+		
+			$this->doRequest($form, $request);
+
+			/*if ($form->isSubmitted()) { //!!! && $form->isValid()
 				// $form->getData() holds the submitted values
         // but, the original `$event` variable has also been updated
-        $event = $form->getData();
+        $event = $form->getData();*/
 
-      // Save the event to the database
-      $entityManager = $this->getDoctrine()->getManager();
-      $entityManager->persist($event);
-      $entityManager->flush();
+      if ($form->isSubmitted()) { //!!! && $form->isValid()
+      	$event = $form->getData();
+		    // Save the event to the database
+		    $entityManager = $this->getDoctrine()->getManager();
+		    $entityManager->persist($event);
+		    $entityManager->flush();
 
-      return $this->redirectToRoute('events');
-			}
+		    return $this->redirectToRoute('events');
+      }
 
 		return $this->render('events/add_event.html.twig', array('form' => $form->createView()));
+	}
+
+	/**
+	 * @Route("/edit/{id}", name="edit")
+	 */
+	public function editAction(Request $request, $id) {
+		//Find the event to update
+		$entityManager = $this->getDoctrine()->getManager();
+		$event = $entityManager->getRepository(Event::class)->find($id);
+
+		if (!$event) {
+			throw $this->createNotFoundException('Event not found with ID: ' . $id);
+		}
+
+		//Read current values
+		$event->setName($event->getName());
+		$event->setImage($event->getImage());
+		$event->setDescription($event->getDescription());
+		$event->setDateTime($event->getDateTime());
+		$event->setCapacity($event->getCapacity());
+		$event->setAddress($event->getAddress());
+		$event->setUrl($event->getUrl());
+		$event->setPhoneNumber($event->getPhoneNumber());
+		$event->setEmail($event->getEmail());
+		$event->setType($event->getType());
+
+		//Try to aupdate the event
+		//Create form
+		$form = $this->createEventForm($event, 'Edit event');
+
+		//Request filled form
+		$this->doRequest($form, $request);
+		//var_dump($form);
+		//die;
+
+		//Get data from form
+		//!!! no validation
+		if ($form->isSubmitted()) { //!!! && $form->isValid()
+    	//Read new values from form
+			$name = $form['name']->getData();
+			$image = $form['image']->getData();
+			$description = $form['description']->getData();
+			$dateTime = $form['datetime']->getData();
+			$capacity = $form['capacity']->getData();
+			$address = $form['address']->getData();
+			$url = $form['url']->getData();
+			$phoneNumber = $form['phoneNumber']->getData();
+			$email = $form['email']->getData();
+			$type = $form['type']->getData();
+
+	    //Set new fields values !!! Set all the fields
+    	$event->setName($name);
+    	$event->setImage($image);
+    	$event->setDescription($description);
+    	$event->setDateTime($dateTime);
+    	$event->setCapacity($capacity);
+    	$event->setAddress($address);
+    	$event->seturl($url);
+    	$event->setPhoneNumber($phoneNumber);
+    	$event->setEmail($email);
+    	$event->setType($type);
+
+			//Update DB record
+			$entityManager->flush();
+
+			//Show info message
+			$this->addFlash('notice', 'Event updated');
+
+			//Redirect to all events
+	    return $this->redirectToRoute('events');
+    }
+		return $this->render('events/edit_event.html.twig', array('form' => $form->createView(), 'event' => $event)); 
 	}
 }
